4f4854a2ce52145313b9b167b8a2aac2cb409ea4
Saulo Caliman <saulocalimangomes@gmail.com>
2023-10-05 19:59:45 -0300

    Diretivas iniciais do projeto

---
+++ ctc-selecao-bolsista/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ctc_selecao_bolsista.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
